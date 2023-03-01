a0211622b706010fc8681fca7a6d46c2a06da9cd
E Akpe <eakpe2020\8@gmail.com>
2023-03-01 20:16:29 +0000

    Backend Initialization

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: function () {
+        return this.role === "seller";
+      },
+    },
+    desc: {
+      type: String,
+      required: function () {
+        return this.role === "seller";
+      },
+    },
+    role: {
+      type: String,
+      enum: ["admin", "buyer", "seller"],
+      default: "buyer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
