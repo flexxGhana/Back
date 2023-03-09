750fca3a480109b148471cf485bab1d885216afc
E Akpe <eakpe2020\8@gmail.com>
2023-03-09 11:06:39 +0000

    **

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const connect = async () => {
     console.log(error);
   }
 };
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://voir-africa.web.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
